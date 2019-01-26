476e0e5c219d424b9a992ba76a2073fababee963
Bruce Tsisen <qzfzz@163.com>
2019-01-26 12:13:25 +0800

    init for qgeo library

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+# coding:utf-8
+
+from setuptools import setup
+
+setup(name='qgeo',
+      version='0.0.1.a',
+      description='qzf geometry library',
+      url='http://github.com/qzfzz/qgeo',
+      author='Ling',
+      author_email='qzfzz@163.com',
+      license='MIT',
+      packages=['qgeo'],
+      zip_safe=False)
